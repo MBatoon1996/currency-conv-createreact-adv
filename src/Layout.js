70de97deb150de53bc02d12190e31f34a43688ca
Marques <MBatoon1996@gmail.com>
2021-03-18 01:53:33 -1000

    added FontAwesome and created footer in Layout component

--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import{ FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+library.add(fab);
 
 const Layout = (props) => {
     return (
@@ -9,7 +14,26 @@ const Layout = (props) => {
             <div className="container py-3">
                 {props.children}
             </div>
-            
+            <footer className="p-3 bg-light">
+                <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 row justify-content-center">
+                    <div id="socialContainer" className="pb-2">
+                        <a href="https://www.instagram.com/batoonworld/" rel="noreferrer" target="_blank" className="mx-1">
+                        <FontAwesomeIcon icon={['fab', 'instagram']} />
+                        </a>
+                        <a href="https://twitter.com/batoonworld" rel="noreferrer" target="_blank" className="mx-1">
+                        <FontAwesomeIcon icon={['fab', 'twitter']} />
+                        </a>
+                        <a href="https://www.github.com/MBatoon1996" rel="noreferrer" target="_blank" className="mx-1">
+                        <FontAwesomeIcon icon={['fab', 'github']} />
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+        <p id="copyright" className="text-center">©2021 Marques Batoon</p>
+      </footer>
         </React.Fragment>
     );
 }
