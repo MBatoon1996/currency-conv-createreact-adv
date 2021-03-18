3f70c0598daa22fcb4fbdf5097708e369262d872
Marques <MBatoon1996@gmail.com>
2021-03-18 02:12:12 -1000

    created currencies object to add to store symbols and fill select options

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Layout from './Layout';
+import Home from './Home';
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route path="/" exact render={() => <Home></Home>} />
           <Route render={() => <h1>404 Not found</h1>} />
         </Switch> 
       </Layout>
